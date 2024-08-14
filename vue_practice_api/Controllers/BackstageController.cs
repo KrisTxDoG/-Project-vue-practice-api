8bf196adf409cfcd0da4572181e3daaf13e134a7
Krist <cysh110254@cy-tech.com>
2024-08-14 14:59:37 +0800

    2024/08/14 - Feature - 新增創建表單功能 by Krist

--- vue_practice_api/Controllers/BackstageController.cs
+++ vue_practice_api/Controllers/BackstageController.cs
@@ -21,5 +21,22 @@ namespace vue_practice_api.Controllers
             var items = await _context.Items.ToListAsync();
             return Ok(items); // 返回 200 狀態碼和資料
         }
+
+        [HttpPost("CreateItem")]
+        public async Task<IActionResult> CreateItem ([FromBody] Item item)
+        {
+            if(ModelState.IsValid)
+            {
+                // 確保 id 為 0，這樣 EF 會將它視為新的項目並讓資料庫自動生成 id
+                item.Id = 0;
+
+                _context.Items.Add(item);
+                await _context.SaveChangesAsync();
+
+                return Ok(item);
+            }
+
+            return BadRequest(ModelState);
+        }
     }
 }
