67798b47019045b7e7fa39282e565a1bc23ef306
Krist <cysh110254@cy-tech.com>
2024-08-16 09:23:12 +0800

    2024/08/16 - Feature - 製作查詢功能 by Krist

--- vue_practice_api/Controllers/BackstageController.cs
+++ vue_practice_api/Controllers/BackstageController.cs
@@ -17,17 +17,26 @@ namespace vue_practice_api.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetItems(int page = 1, int pageSize = 10)
+        public async Task<IActionResult> GetItems(int page = 1, int pageSize = 10, string search= "")
         {
             // 確保頁碼和頁面大小為有效值
             page = page < 1 ? 1 : page;
             pageSize = pageSize < 1 ? 10 : pageSize;
 
+            // 準備查詢
+            var query = _context.Items.AsQueryable();
+
+            // 如果提供了搜尋字串, 則進行篩選
+            if (!string.IsNullOrEmpty(search))
+            {
+                query = query.Where(item => item.Name.Contains(search));
+            }
+
             // 獲取總的項目數量
-            var totalItems = await _context.Items.CountAsync();
+            var totalItems = await query.CountAsync();
 
             // 根據頁碼和頁面大小獲取數據
-            var items = await _context.Items
+            var items = await query
                 .OrderBy(x => x.CreatedDate)
                 .Skip((page -1) * pageSize)
                 .Take(pageSize)
@@ -44,7 +53,6 @@ namespace vue_practice_api.Controllers
         {
             if(ModelState.IsValid)
             {
-                // 確保 id 為 0，這樣 EF 會將它視為新的項目並讓資料庫自動生成 id
                 item.Id =  Guid.NewGuid();
                 item.CreatedDate = DateTime.Now;
 
